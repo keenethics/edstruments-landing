847569c113142f7cec050d93374ef9297ba3c793
antonmorya <antonmorya@gmail.com>
2019-08-09 11:31:23 +0300

    Navbar made fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import "./sass/main.scss";
-// const $ = require("jquery");
-// $(".carousel").carousel();
 
+function scrollPositionCheck() {
+  if (window.scrollY < 10) {
+    document.querySelector(".hero .navbar").classList.remove("detached");
+  } else {
+    document.querySelector(".hero .navbar").classList.add("detached");
+  }
+}
 
+document.addEventListener(
+  "DOMContentLoaded",
+  function(event) {
+    scrollPositionCheck();
+  },
+  false
+);
+
+window.addEventListener("scroll", function() {
+  scrollPositionCheck();
+});
